e8237d58c1d2d368c533a57167b8d4d53a82bc12
Jeff Miller <jeffmax@gmail.com>
2015-05-06 20:19:08 -0400

    Add first python 'hello world' script

---
+++ start.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# This is a comment. 
+# Lines beginning with a # are completely ignored by Python
+
+
+# Below is a string
+"<!DOCTYPE html><html><body><h1>"
+
+# This is another one
+"</h1></body></html>"
+
+# Together these are almost an HTML document like we observed in the browser
+
+# Exercise 1
+# Add another string in the middle to make the title of the page "Patient Record"
+
+# Let's run this program
+
+# Exercise 2
+# Make the program print out to the screen when we run it
+
+# Exercise 3
+# Clean this program up a bit by introducing variables
