bb13ae41c7c98e576ba123194969216b21807162
Jeffrey Miller <millerjm1@email.chop.edu>
2015-05-08 09:42:39 -0400

    HTML syntax

--- portal.py
+++ portal.py
@@ -9,7 +9,7 @@ print('')
 print("""<!DOCTYPE html>
     <html>
         <head>
-        <link rel="stylesheet" type="text/css" href="/css/main.css">
+        <link rel=stylesheet type=text/css href=/css/main.css>
         </head>
         <body>""")
 
